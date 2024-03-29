8a84405e3b907b4ee490cba5d8eab9b18a6b808d
Nicolas Corbalan <outreef@hotmail.com>
2024-03-29 14:08:48 +0100

    creating a generate random color

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
 import Accordian from "./components/accordian";
+import RandomColor from "./components/random-color";
 
 function App() {
   return (
     <div className="App">
       <Accordian />
+      {/*Random Color Component */}
+      <RandomColor />
     </div>
   );
 }
