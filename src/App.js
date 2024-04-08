2e7de4f5dea18e3d073ef88e2b7314b5a2cc6926
Nicolas Corbalan <outreef@hotmail.com>
2024-04-08 15:46:45 +0200

    review stars clicking and hover them

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Accordian from "./components/accordian";
 import RandomColor from "./components/random-color";
+import StarRating from "./components/star-rating";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Accordian />
       {/*Random Color Component */}
       <RandomColor />
+      <StarRating noOfStars={10} />
     </div>
   );
 }
